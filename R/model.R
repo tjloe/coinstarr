8f06e4b712da78f959f9816a6106d8bdf6333daf
Tyler J Loewenstein <tylerjloewenstein@gmail.com>
2018-12-12 05:43:23 -0500

    model broken

--- R/model.R
+++ R/model.R
@@ -8,24 +8,26 @@
 #'
 #' @param df A data frame
 #' @param formula An object of class "formula"
-#' @param filter A logical test to filter observations in the data frame
+#' @param sift A logical test to filter observations in the data frame
 #'
 #' @export
 
-model <- function(df, formula, filter = NULL){
+model <- function(df, formula, sift = NULL){
 
   if(!class(formula) == "formula"){
     stop("formula argument is not valid.")
   }
 
-  if(is.null(filter)){
+  if(is.null(sift)){
     out <- lm(formula, data = df)
   } else {
 
-    out <- lm(formula, data = dplyr::filter(df, filter))
+    df2 <- subset(df, eval(parse(text = sift)))
+    out <- lm(formula, data = df2)
 
   }
 
+return(out)
 
 }
 
