ea92b5e9470ef804093235f8bd414179bbd33d40
Tyler J Loewenstein <tylerjloewenstein@gmail.com>
2018-12-12 03:19:17 -0500

    back to pol method

--- R/collect.R
+++ R/collect.R
@@ -9,20 +9,20 @@
 #' @param df a data frame
 #' @param group a grouping variable
 #' @param by a second grouping variable different from \code{group}
-#' @param method a method specifying what type of data is being used. Can be either "position" or "hours".
+#' @param method a method specifying what type of data is being used. Can be either "position", "overtime", or "leave".
 #'
 #'
 #' @examples
 #' collect(jobs, "agency", method = "position")
-#' collect(overtime, "paygroup", method = "hours")
-#' collect(leave, "type", method = "hours")
+#' collect(overtime, "paygroup", method = "overtime")
+#' collect(leave, "type", method = "leave")
 #'
 #' @export
 
 collect <- function(df, group, by = "payday", method) {
   `%>%` <- magrittr::`%>%`
 
-  if (!method %in% c("position", "hours")) {
+  if (!method %in% c("position", "overtime", "leave")) {
     stop("unknown method")
   }
 
